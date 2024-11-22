265c2c9f18d851c569cb9d3278c6615893041a04
Kieran-McD <1903369@uad.ac.uk>
2024-11-22 18:07:37 +0000

    Exercise 1: Find Max Height

--- Assets/Exercise 1/Scripts/MathPredictions.cs
+++ Assets/Exercise 1/Scripts/MathPredictions.cs
@@ -7,8 +7,12 @@ public static class MathPredictions
     public static bool TryCalculateXPositionAtHeight(float h, Vector2 p, Vector2 v, float G, float w, ref float xPosition)
     {
         //Check if ball reaches height
+        float maxHeight = ((v.y * v.y) / -G) * 0.5f + p.y;
+        Debug.Log("Max Height: " + maxHeight);
+        if (maxHeight < h) return false;
 
         //Find Time Takes To Reach Height
+
 
         //Use time to find how far the ball goes in the X Axis
 
