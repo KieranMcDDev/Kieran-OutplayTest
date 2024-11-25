cc0296808f314a2444ab2fddb7c5fe781c43c112
Kieran-McD <1903369@uad.ac.uk>
2024-11-25 15:41:06 +0000

    Exercise 3: Setup

---
+++ Assets/Exercise 3/Scripts/MainObject.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class MainObject : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform[] points = new Transform[3];
+
+    public void Update()
+    {
+        
+    }
+
+
+    //Move to current point
+    private void MoveTo()
+    {
+        
+    }
+}
+
---
+++ Assets/Exercise 3/Scripts/Obstacle.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+   
+    public void Awake()
+    {
+        
+    }
+
+    //Handle spawning the game object
+    public void Spawn()
+    {
+
+    }
+}
