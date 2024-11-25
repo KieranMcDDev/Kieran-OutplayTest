2e75a63a470b82b1ec2ebe31579a4744796f5f43
Kieran-McD <1903369@uad.ac.uk>
2024-11-25 16:19:45 +0000

    Exercise 3: Main Object Moves to Points

--- Assets/Exercise 3/Scripts/MainObject.cs
+++ Assets/Exercise 3/Scripts/MainObject.cs
@@ -1,8 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
+﻿using UnityEditor;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
+
 
 public class MainObject : MonoBehaviour
 {
@@ -10,16 +8,62 @@ public class MainObject : MonoBehaviour
     [SerializeField]
     Transform[] points = new Transform[3];
 
+    [SerializeField]
+    int index;
+
+    [SerializeField]
+    float speed;
+
+    private void Start()
+    {
+        index = 0;
+    }
     public void Update()
     {
-        
+        if(index < points.Length)
+        {
+            MoveTo();
+        }
     }
-
 
     //Move to current point
     private void MoveTo()
     {
+        transform.position = Vector3.MoveTowards(transform.position, points[index].position, speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, points[index].position) == 0)
+        {
+            index++;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        // Draw a yellow cube at the transform position
+
+        if (points.Length < 1) return;
+        if (points[0] != null)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(transform.position, points[0].position);
+        }
+
+        for(int i = 0;i < points.Length-1; i++)
+        {
+            if (points[i] != null)
+            {
+                Gizmos.color = Color.green;
+                Gizmos.DrawSphere(points[i].position, 0.2f);
+
+                if (points[i + 1])
+                {
+                    Gizmos.DrawSphere(points[i + 1].position, 0.2f);
+                    Gizmos.color = Color.blue;
+                    Gizmos.DrawLine(points[i].position, points[i + 1].position);
+                }
+            }
+        }
+
         
     }
 }
-
